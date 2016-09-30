1c312742c31bddbfbb0bd32ebee4ef685ef390a0
Daniel Kwolek <Daniel@Daniels-MacBook-Pro.local>
2016-09-30 16:32:17 -0400

    Networking functionality implemented

--- Rocket/AppDelegate.swift
+++ Rocket/AppDelegate.swift
@@ -9,8 +9,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        try? WebLink.lookUpUserWithIDTest()
+        // try? WebLink.registerUserWithIDTest()
         return true
+        
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
--- Rocket/WebLink.swift
+++ Rocket/WebLink.swift
@@ -5,6 +5,14 @@ import Foundation
 /// Various functions using different inputs to return "User" or "Event" Objects based on various queries
 class WebLink {
     
+    static var baseURL: String = "http://10.0.0.138:8080"
+    
+    public enum webMethods: String {
+        case login = "/login.json"
+        case register = "/register.json"
+    }
+    
+
     public enum lada: Error {
         case invalidEmailOrPassword(String)
         case invalidID
@@ -12,11 +20,51 @@ class WebLink {
         case invalidUser
     }
     
-    static func fetchUserWithID(userid: UUID) throws -> User? {
+    static func lookUpUserWithIDTest() throws {
         let session = URLSession.shared
-        var url: URL
-        var urlrequest: URLRequest
+        let url: URL = URL(string: "\(baseURL)\(webMethods.login.rawValue)")!
+        var urlrequest: URLRequest = URLRequest.init(url: url)
+        urlrequest.httpMethod = "POST"
+        urlrequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        urlrequest.addValue("application/json", forHTTPHeaderField: "Accept")
         
+        let emailpassword: [String: String] = ["password" : "Test1Test1", "email" : "TestEmail"]
+        
+        do {
+            urlrequest.httpBody = try JSONSerialization.data(withJSONObject: emailpassword, options: [])
+            let task = session.dataTask(with: urlrequest)  { (data: Data?, response: URLResponse?, error: Error?) in
+                if data != nil {
+                    print("\n\n\n\n We got \(data)\n\n\n\n ")
+                }
+            }
+            task.resume()
+        } catch let errorthing {
+            throw errorthing
+        }
+        throw lada.invalidEmailOrPassword("Invalid Email Or Password")
+    }
+    
+    static func registerUserWithIDTest() throws {
+        let session = URLSession.shared
+        let url: URL = URL(string: "\(baseURL)\(webMethods.register.rawValue)")!
+        var urlrequest: URLRequest = URLRequest.init(url: url)
+        urlrequest.httpMethod = "POST"
+        urlrequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        urlrequest.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        let emailpassword: [String: String] = ["password" : "Test2Test2", "email" : "TestEmail2"]
+        
+        do {
+            urlrequest.httpBody = try JSONSerialization.data(withJSONObject: emailpassword, options: [])
+            let task = session.dataTask(with: urlrequest)  { (data: Data?, response: URLResponse?, error: Error?) in
+                if data != nil {
+                    print("\n\n\n\n We got \(data)\n\n\n\n ")
+                }
+            }
+            task.resume()
+        } catch let errorthing {
+            throw errorthing
+        }
         throw lada.invalidEmailOrPassword("Invalid Email Or Password")
     }
     
