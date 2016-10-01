647ab762e619c6167ba5f8b71a6e1f556b765d4b
Neil <vitalene@msu.edu>
2016-10-01 10:29:49 -0400

    committing some changes to the EventListViewController

--- Rocket/EventListTableViewController.swift
+++ Rocket/EventListTableViewController.swift
@@ -4,16 +4,32 @@ import UIKit
 
 class EventListTableViewController: UITableViewController {
     
+    var eventStore: EventStore!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "New Events"
     }
+
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return EventStore.allTheEvents.count
     }
     
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Create an instance of UITableViewCell, with default appearance
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell",
+                                                 for: indexPath)
+        
+        // Set the text on the cell with the description of the item
+        // that is at the nth index of items, where n = row this cell
+        // will appear in on the tableview
+        let item = eventStore.allTheEvents?[(indexPath as NSIndexPath).row]
+  
+        
+        return cell
+    }
     
 }
 
--- Rocket/EventStore.swift
+++ Rocket/EventStore.swift
@@ -3,12 +3,20 @@
 import Foundation
 
 struct EventStore {
+    var allTheEvents: [Event]?
+    // number of events in the Event list View
+    
     static var myEvents: [Event]?
+    // number of events that have been "checked into"
     
     static var tempEvents: [Event] = [EventStore.tempEvent1, EventStore.tempEvent2, EventStore.tempEvent3]
     
     static var tempEvent1 = Event.createEventWithData(name: "Test1", location: "Test1Loc", attendees: [UserStore.testUser1.email!, UserStore.testUser2.email!, UserStore.testUser3.email!], eventID: UUID.init(), description: "Test1Desc")
+    
+    
     static var tempEvent2 = Event.createEventWithData(name: "Test2", location: "Test2Loc", attendees: [UserStore.testUser1.email!, UserStore.testUser2.email!, UserStore.testUser3.email!], eventID: UUID.init(), description: "Test2Desc")
+    
+    
     static var tempEvent3 = Event.createEventWithData(name: "Test3", location: "Test3Loc", attendees: [UserStore.testUser1.email!, UserStore.testUser2.email!, UserStore.testUser3.email!], eventID: UUID.init(), description: "Test3Desc")
     
     static func addEventToStore(event: Event) {
